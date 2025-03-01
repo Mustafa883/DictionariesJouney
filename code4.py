850fdae56961def4077c035e58d3fedc9b58a782
Mustafa Al-Omari <mustafaalomari@gmail.com>
2025-03-01 13:50:39 +0200

    Creating for loop to see if value are in the dictionary.txt

--- code4.py
+++ code4.py
@@ -3,3 +3,9 @@ def rearrange(dictionary):
      invert = {
         
     }
+    for key, value in dictionary.items():
+        if value in invert:
+            invert[value].append(key)
+        else:
+            invert[value] = [key]
+    return invert
