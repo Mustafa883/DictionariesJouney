c88039d747c8595f38239f905664f2e5f0692ae1
Mustafa Al-Omari <mustafaalomari@gmail.com>
2025-03-01 13:36:50 +0200

    Printing the employees.txt

--- code3.py
+++ code3.py
@@ -1,1 +1,21 @@
 print("Part3: ")
+employees = {
+    "Engineering Department": {
+        "Alice": {
+            "Age": 30,
+            "Role": "Software Engineer",
+        },
+        "Bob": {
+            "Age": 28,
+            "Role": "DevOps Engineer",
+        },
+    },
+    "HR Deprtment": {
+        "Charlie": {
+            "Age": 35,
+            "Role": "HR Manager",
+        }
+    }
+}
+print("The company employees are: ")
+print(employees)
