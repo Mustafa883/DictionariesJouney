22c419520ca579d73ffe54ab971483e58f417c4f
Mustafa Al-Omari <mustafaalomari@gmail.com>
2025-03-01 13:16:46 +0200

    Creating mergeddictionaries that takes dict1 & dict2 and returns them combined with no duplications.txt

--- code1.py
+++ code1.py
@@ -8,3 +8,8 @@ dict2 = {
     "b":10,
     "d":4,
 }
+def mergeddictionaries(dict1,dict2):
+    mergedict = dict1.copy()
+    mergedict.update(dict2)
+    return(mergedict)
+mergedresults = mergeddictionaries(dict1,dict2)
